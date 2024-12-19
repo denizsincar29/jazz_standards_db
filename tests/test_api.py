ae4dd4fbdc1f365a8795c98befc0bea19814f6f9
deniz <deniz@r1oaz.ru>
2024-12-19 16:05:21 +0300

    Use session fixture in test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import pytest
 import base64
 import os
 try:
@@ -15,7 +16,7 @@ BASIC_AUTH = base64.b64encode(b"auth_admin:qwerty").decode("utf-8")
 # Create the first user for authentication
 def create_or_get_auth_user(is_admin=True):
     auth_username = "auth_admin" if is_admin else "auth_user"
-    response = client.get(f"/api/users/me", headers={"Authorization": f"Basic {BASIC_AUTH}"})
+    response = client.get("/api/users/me", headers={"Authorization": f"Basic {BASIC_AUTH}"})
     if response.status_code == 200:
         print("got the user", response.cookies)
         return response.json(), response.cookies
@@ -39,9 +40,23 @@ def get_auth_headers(is_admin=True):
     auth_user, delicious_cookies = create_or_get_auth_user(is_admin)  # cookies are delicious, aren't they?
     return {"Authorization": f"Bearer {delicious_cookies['cookie_token']}"}
 
-def test_read_root():
-    headers = get_auth_headers()
-    response = client.get("/api/", headers=headers)
+@pytest.fixture(scope="session")
+def headers():
+    print("Setting up the test db")
+    # Create the first user for authentication
+    admin = get_auth_headers(True)
+    user = get_auth_headers(False)
+    yield admin, user
+    print("Tearing down the test db")
+    client.close()
+    try:
+        os.remove("test.db")
+    except FileNotFoundError:
+        pass
+
+def test_read_root(headers):
+    admin, user = headers
+    response = client.get("/api/", headers=admin)
     assert response.status_code == 200
     # well, check that the response is a dict but not the content, and it has keys users and jazz standards
     assert isinstance(response.json(), dict)
@@ -49,7 +64,6 @@ def test_read_root():
     assert "jazz_standards" in response.json()
 
 def test_create_user():
-    #headers = get_auth_headers()  # or it could be a chane: to register you need to be registered, lol
     response = client.post(
         "/api/users/",
         json={"username": "test_user", "name": "Test User", "password": "securepass", "is_admin": True},
@@ -57,24 +71,22 @@ def test_create_user():
     assert response.status_code == 200
     assert response.json() == {"id": 2, "username": "test_user", "name": "Test User", "is_admin": True}
 
-def test_read_user():
-    headers = get_auth_headers()
+def test_read_user(headers):
     response = client.post(
         "/api/users/",
         json={"username": "other_user", "name": "Other User", "password": "securepass2", "is_admin": False},
-        headers=headers
+        headers=headers[0]
     )
     user_id = response.json()["id"]
     response = client.get(f"/api/users/{user_id}", headers=headers)
     assert response.status_code == 200
     assert response.json() == {"id": user_id, "username": "other_user", "name": "Other User", "is_admin": False}
 
-def test_read_users():
-    headers = get_auth_headers()
+def test_read_users(headers):
     client.post(
         "/api/users/",
         json={"username": "test_user1", "name": "Test User 1", "password": "securepass3", "is_admin": True},
-        headers=headers
+        headers=headers[0]
     )
     client.post(
         "/api/users/",
@@ -88,16 +100,14 @@ def test_read_users():
     j = response.json()
     assert {"id": 3, "username": "test_user1", "name": "Test User 1", "is_admin": True} in j
     assert {"id": 4, "username": "test_user2", "name": "Test User 2", "is_admin": False} in j
-    headers = get_auth_headers(False)  # nope, you're not admin
-    response = client.get("/api/users/", headers=headers)
+    response = client.get("/api/users/", headers=headers[1])  # non admin user
     assert response.status_code == 401  # yikes! you're not admin, debil :D
 
-def test_delete_user():
-    headers = get_auth_headers()
+def test_delete_user(headers):
     response = client.post(
         "/api/users/",
         json={"username": "delete_user", "name": "Delete User", "password": "securepass5", "is_admin": False},
-        headers=headers
+        headers=headers[0]
     )
     j = response.json()
     print(j)
